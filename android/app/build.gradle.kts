ca2b42414488b6454cf94d7573e103097b0d5e4e
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2026-04-27 10:32:31 +0200

    fix: correct application ID format in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "de.gbs-cidp.cidpbuddy"
+        applicationId = "de.gbs_cidp.cidpbuddy"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
